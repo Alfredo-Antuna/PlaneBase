f4797fb637b29220a492a75266b2b993f82dfd66
Alfredo Antuna <Alfredo.Antuna@lntinfotech.com>
2021-09-30 10:45:41 -0500

    added command line interface long

--- app/Program.cs
+++ app/Program.cs
@@ -10,10 +10,105 @@ namespace app
         static void Main(string[] args)
         {
             using var Context = new Context();
-            //Search for flights
-            ChangeInfo.ShowSeats("3",Context);
-            ChangeInfo.BuySeat("16",Context);
-            ChangeInfo.ShowSeats("3",Context);
+            var cliControl = true;
+            ChangeInfo.ShowFlights(Context);
+            while(cliControl)
+            {
+                Console.WriteLine(@"
+                Input 0 for Admin Options
+                Input 1 to Purchase Seat
+                press Enter to exit
+                ");
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    
+                    case "0":
+                    {
+                        Console.WriteLine(@"
+                        Input 0 to see how full each flight is (in percentage)
+                        Input 1 to see how many kids in total have seats across all flights
+                        Input 2 to see see the total amount of revenue per flight
+                        Input 3 Create a Flight
+                        ");
+                        option = Console.ReadLine();
+                        if(option == "0")
+                        {
+                            Console.WriteLine("Input Flight id to view");
+                            var id = Console.ReadLine();
+                            Console.WriteLine(ChangeInfo.ShowFlightFullPercent(id,Context) +"%");
+                        }
+                        else if(option == "1")
+                        {
+                             Console.WriteLine(ChangeInfo.ShowChildCount(Context));
+                        }
+                        else if (option == "2")
+                        {
+                             Console.WriteLine(ChangeInfo.ShowTotalRevenue(Context));
+                        }
+                        else if (option == "3")
+                        {
+                            Console.WriteLine("Input [Aircraft Departure Arrival FirstClassSeats EconomyClassSeats BusinessClassSeats]");
+                                var input = Console.ReadLine();
+                                var inputParsed = input.Split(" ");
+                                ChangeInfo.AddFlight(inputParsed[0],inputParsed[1],inputParsed[2],Convert.ToInt32(inputParsed[3]),Convert.ToInt32(inputParsed[4]),Convert.ToInt32(inputParsed[5]),Context);
+                        }
+                         
+                        
+                        break;
+
+
+                        //AddFlight(string inputAircraft,string inputDeparture,string inputArrival,int inputFirstClass,int inputEconomyClass,int inputBusinessClass
+                    }
+                
+                    case "1":
+                    {
+                        var customerLoopControl = true;
+
+
+                        while(customerLoopControl)
+                        {
+                            //ChangeInfo.ShowFlights(Context);
+                            Console.WriteLine(@"
+                            Input 0 to Search Flights
+                            Input 1 to Buy a seat by Id
+                            Press enter to go back
+                            ");
+                            option = Console.ReadLine();
+                            if(option == "0")
+                            {
+                                Console.WriteLine("Input Search Option[Aircraft,Arrival,Departure], SearchWord");
+                                var search = Console.ReadLine();
+                                var searchParsed = search.Split(" ");
+                                ChangeInfo.SearchFlight(searchParsed[0],searchParsed[1],Context);
+                            }
+                            else if(option == "1")
+                            {
+                                Console.WriteLine("Input Flight Id to see Available seats");
+                                var search = Console.ReadLine();
+                                ChangeInfo.ShowSeats(search,Context);
+                                Console.WriteLine("Input Seat Id to purchase");
+                                search = Console.ReadLine();
+                                ChangeInfo.BuySeat(search,Context);
+                            }
+                            else
+                            {
+                                customerLoopControl = false;
+                            }  
+                        }
+                        break;
+                    }
+                
+                    case "":
+                    {
+                        cliControl = false;
+                        break;
+                    }
+                }
+
+
+            }
 
         }
     }
--- lib/ChangeInfo.cs
+++ lib/ChangeInfo.cs
@@ -15,17 +15,17 @@ namespace lib
             Flight CreatedFlight =
              new Flight {Aircraft = inputAircraft,Departure = inputDeparture, Arrival = inputArrival,FirstClass = inputFirstClass, EconomyClass = inputEconomyClass,BusinessClass = inputBusinessClass };
             var NewSeats = new List<Seat>();
-            for(int Count = 1;Count<inputEconomyClass;Count++)
+            for(int Count = 1;Count<=inputEconomyClass;Count++)
             {
-                NewSeats.Add(new Seat(){ Name = $"B{Count}", Cost = 8,Class = "EcomonyClass",Flight = CreatedFlight});
+                NewSeats.Add(new Seat(){ Name = $"B{Count}", Cost = 8,Class = "EcomonyClass",CustomerType = "Empty",Flight = CreatedFlight});
             }
-            for(int Count = 1;Count<inputFirstClass;Count++)
+            for(int Count = 1;Count<=inputFirstClass;Count++)
             {
-                NewSeats.Add(new Seat(){ Name = $"A{Count}", Cost = 15,Class = "FirstClass",Flight = CreatedFlight});
+                NewSeats.Add(new Seat(){ Name = $"A{Count}", Cost = 15,Class = "FirstClass",CustomerType = "Empty",Flight = CreatedFlight});
             }
-            for(int Count = 1;Count<inputBusinessClass;Count++)
+            for(int Count = 1;Count<=inputBusinessClass;Count++)
             {
-                NewSeats.Add(new Seat(){ Name = $"C{Count}", Cost = 10,Class = "BusinessClass",Flight = CreatedFlight});
+                NewSeats.Add(new Seat(){ Name = $"C{Count}", Cost = 10,Class = "BusinessClass",CustomerType = "Empty",Flight = CreatedFlight});
             }
 
             CreatedFlight.Seats.AddRange(NewSeats);
@@ -79,23 +79,28 @@ namespace lib
                 
             }
         }
+        public static void ShowFlights(Context Context)
+        {
+            var results = Context.Flights;
+            foreach (var flight in results)
+            {
+                Console.WriteLine($"ID:{flight.ID} - Aircraft: {flight.Aircraft} Departure: {flight.Departure} - Arrival: {flight.Arrival}");
+                Console.WriteLine($"Total Seats[FirstClass: {flight.FirstClass} - BusinessClass: { flight.BusinessClass} - EconomyClass: {flight.EconomyClass}");
+            }
+        }
         public static void ShowSeats(string ID,Context Context)
         {
             var result = Context.Flights.Where(flight => flight.ID == Convert.ToInt32(ID)).Include(fli => fli.Seats).First();
             Console.WriteLine($"Total Seats[FirstClass: {result.FirstClass} - BusinessClass: { result.BusinessClass} - EconomyClass: {result.EconomyClass}");
             Console.WriteLine($"Available Seats:");
-            string Occupied;
             foreach (var seat in result.Seats)
             {
                 
                 if(seat.Occupied == 0)
                 {
-                    Occupied = "AVAILABLE";
-                }else
-                {
-                    Occupied = "UNAVAILABLE";
+                    Console.WriteLine($"Name:{seat.Name} - ID:{seat.ID} - Vacancy:AVAILABLE - Cost:{seat.Cost} - Class:{seat.Class}" );
                 }
-                Console.WriteLine($"Name:{seat.Name} - ID:{seat.ID} - Vacancy:{Occupied} - Cost:{seat.Cost} - Class:{seat.Class}" );
+              
             }
                         
                     
@@ -113,7 +118,37 @@ namespace lib
             }
 
         }
+        public static decimal ShowFlightFullPercent(String ID,Context Context)
+        {
+            int totalSeats = 0;
+            int OccupiedSeats = 0;
+            var result = Context.Flights.Where(flight => flight.ID == Convert.ToInt32(ID)).Include(fli => fli.Seats).First();
+            totalSeats = result.EconomyClass + result.BusinessClass + result.FirstClass;
+            foreach (var chair in result.Seats)
+            {
+                if(chair.Occupied == 1){OccupiedSeats++;}
+            }
+            var percent = (decimal)OccupiedSeats/(decimal)totalSeats;
+            return Math.Round(percent,2)* 100;
 
+        }
+        public static int ShowChildCount(Context Context)
+        {
+            var results = Context.Seats.Where(seat => seat.CustomerType == "Child");
+            
+            return results.Count();
+        }
+        public static int ShowTotalRevenue(Context Context)
+        {
+            
+            int totalRevenue = 0;
+            var results = Context.Seats.Where(seat => seat.Occupied == 1);
+            foreach (var seat in results)
+            {
+                totalRevenue += seat.Cost;
+            }
+            return totalRevenue;
 
+        }
     }
 }
